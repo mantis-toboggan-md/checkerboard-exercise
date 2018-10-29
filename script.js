4ef4d5a1403dae63b4dd0e66628bc46364ca12a0
Nancy <butlern0513@gmail.com>
2018-10-29 13:50:59 -0700

    make black/red checkerboard in js

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
-// Your JS goes here
+var tile = document.createElement("div");
+Object.assign(tile.style,{"width" : "11.1%", "float" : "left", "padding-bottom" : "11.1%", "background-color" : "red"});
+for(var i = 0; i<100; i++){
+  if(i & 1){
+    Object.assign(tile.style, {"background-color" : "black"});
+  }
+  else{
+    Object.assign(tile.style, {"background-color" : "red"});
+  }
+  document.body.appendChild(tile.cloneNode(true));
+}
