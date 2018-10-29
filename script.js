0b021e7809a3c90b3cc59b027b91d34fc75ee6c7
Nancy <butlern0513@gmail.com>
2018-10-29 14:04:46 -0700

    added randomly colored checkboard using js

--- script.js
+++ script.js
@@ -1,11 +1,6 @@
 var tile = document.createElement("div");
-Object.assign(tile.style,{"width" : "11.1%", "float" : "left", "padding-bottom" : "11.1%", "background-color" : "red"});
+Object.assign(tile.style,{"width" : "11.1%", "float" : "left", "padding-bottom" : "11.1%"});
 for(var i = 0; i<100; i++){
-  if(i & 1){
-    Object.assign(tile.style, {"background-color" : "black"});
-  }
-  else{
-    Object.assign(tile.style, {"background-color" : "red"});
-  }
+  tile.style.backgroundColor =  `rgb(${Math.random() * 220}, ${Math.random() * 220}, ${Math.random() * 220})`;
   document.body.appendChild(tile.cloneNode(true));
 }
